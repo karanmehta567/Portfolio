0def823644e03e92e566d5bcfaac4c8639057046
karanmehta567 <karanmehta29.km@gmail.com>
2025-05-21 16:27:26 +0530

    feat: add @vercel/analytics dependency and integrate Analytics component in App

--- port/src/App.jsx
+++ port/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import NotFound from "./pages/notFound.jsx";
+import { Analytics } from "@vercel/analytics/next";
 import { Toaster } from "./components/ui/toaster.jsx";
 
 function App() {
   return (
     <>
+      <Analytics />
       <Toaster />
       <BrowserRouter>
         <Routes>
