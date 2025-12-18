b324359e76314f72054ab5ffc7704db53088cf87
karanmehta567 <karanmehta29.km@gmail.com>
2025-12-18 22:17:45 +0530

    Created Project Section-Updated design

--- port/src/components/ProjectSection.jsx
+++ port/src/components/ProjectSection.jsx
@@ -3,48 +3,39 @@ import { ArrowRight, ExternalLink, Github } from "lucide-react";
 const projects = [
   {
     id: 1,
-    title: "SaaS Landing Page",
+    title: "AI Interview Recruiter",
     description:
-      "Built a responsive dark-themed AI website using React and Tailwind CSS with a clean, modern UI",
-    image: "/project/reavy.png",
-    tags: ["React", "TailwindCSS", "Supabase"],
-    demoUrl: "https://landing-react-randomai.vercel.app/",
-    githubUrl: "https://github.com/karanmehta567/Landing-react",
+      "Built AI interview recruiter application using Nextjs,OpenAI which lets you mock interviews using text to speech recognition(VAPI) and also provides feedback related to the selected domain",
+    image: "/project/interview.png",
+    tags: ["Nextjs","Vapi", "TailwindCSS", "Supabase-Auth"],
+    demoUrl: "https://interview-project-khaki.vercel.app/auth",
+    githubUrl: "https://github.com/karanmehta567/interview-project",
   },
   {
     id: 2,
-    title: "CloudShipX",
+    title: "ElysiaChat",
     description:
-      "Vercel Clone replicates your project files to an S3 bucket, retrieves them locally to build the project, and then deploys the required build artifacts back to S3. Redis is used to manage real-time continuation messages, such as upload status and deployment progress",
-    image: "/project/project1.png",
-    tags: ["Typescript", "React", "AWS-SDK", "Redis"],
-    githubUrl: "https://github.com/karanmehta567/CloudShipX",
-  },
-  {
-    id: 3,
-    title: "Travel Planner",
-    description:
-      "It lets you plan your upcoming travelling journey,create itineraries and pinpoint the location on globe",
-    image: "/project/travel.png",
-    tags: ["NextJS", "Typescript", "PostgreSQL", "OAuth"],
-    githubUrl: "https://github.com/karanmehta567/travel-planner",
-    demoUrl: "https://travel-planner-murex.vercel.app/",
+      "Real Time Chat Application created using Nextjs,Tanstack Query,Zod,Upstash Redis which lets you chat in real time and also self destroys the room after TTL expiration",
+    image: "/project/chat.png",
+    tags: ["Typescript", "Nextjs", "TanStack Query", "Upstash-Redis"],
+    demoUrl: "https://chat-message-elysia.vercel.app/",
+    githubUrl: "https://github.com/karanmehta567/redis-messaging-elysia",
   },
 ];
 
 export const ProjectSection = () => {
   return (
-    <section id="projects" className="py-24 px-4 relative">
-      <div className="container mx-auto max-w-3xl">
+    <section id="projects" className="py-24 px-10 relative">
+      <div className="container mx-auto max-w-4xl">
         <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center">
           {" "}
           Featured <span className="text-primary"> Projects </span>
         </h2>
 
-        <p className="text-center text-muted-foreground mb-12 max-w-2xl mx-auto">
+        <p className="text-center text-muted-foreground mb-12 max-w-3xl mx-auto">
           Here are some of my recent projects.
         </p>
-        <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8">
           {projects.map((project, key) => (
             <div
               key={key}
@@ -59,9 +50,9 @@ export const ProjectSection = () => {
               </div>
 
               <div className="p-6">
-                <div className="flex flex-wrap gap-2 mb-4">
+                <div className="flex flex-wrap gap-2 mb-4 items-center justify-between">
                   {project.tags.map((tag) => (
-                    <span className="px-2 py-1 text-xs font-medium border rounded-full bg-secondary text-secondary-foreground">
+                    <span className="px-2 py-1 text-xs font-medium border rounded-full bg-secondary text-secondary-foreground hover:bg-blue-400">
                       {tag}
                     </span>
                   ))}
@@ -71,8 +62,8 @@ export const ProjectSection = () => {
                 <p className="text-muted-foreground text-sm mb-4">
                   {project.description}
                 </p>
-                <div className="flex justify-between items-center">
-                  <div className="flex space-x-3">
+                <div className="flex mt-8 items-end justify-end">
+                  <div className="flex items-center justify-between gap-x-5 flex-1/2">
                     {project.demoUrl && (
                       <a
                         href={project.demoUrl}
